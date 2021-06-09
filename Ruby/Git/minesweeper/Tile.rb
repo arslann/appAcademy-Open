a8a7e905f82ffa1cb2fc4b59eb2ab25d5d68c8ae
arslann <arslansahin.1907@gmail.com>
2021-06-09 22:14:00 +0300

    add reveal, flag methods

--- Ruby/Git/minesweeper/Tile.rb
+++ Ruby/Git/minesweeper/Tile.rb
@@ -1,5 +1,22 @@
 class Tile
     def initialize(bomb)
         @isBomb = bomb
+        @flagged = false
+        @revealed = false
     end
+
+    def reveal
+        @revealed = true
+    end
+
+    def flag
+        @flagged = true
+    end
+
+    def unflag
+        @flagged = false
+    end
+
+    
+
 end
